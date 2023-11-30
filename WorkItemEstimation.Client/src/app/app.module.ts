68777f341d425eba2246c9d20a0ca637589e9ca8
Mina Ibrahim <iar1fe@bosch.com>
2023-11-30 15:38:51 +0100

    add header component

--- WorkItemEstimation.Client/src/app/app.module.ts
+++ WorkItemEstimation.Client/src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateOrJoinRoomComponent } from './create-or-join-room/create-or-join-room.component';
+import { HeaderComponent } from './header/header.component';
+import { MaterialModule } from './material/material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateOrJoinRoomComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    MaterialModule,
     BrowserAnimationsModule,
   ],
   providers: [],
